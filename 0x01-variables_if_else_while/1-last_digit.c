e3ff7f56e831f2430fd832afa20f2345bf9bbe94
Birone Agutu <agutubiron@gmail.com>
2023-10-09 13:33:52 +0300

    hellow world

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,12 +17,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	m=n%10;
-	if(m>5)
+	m=n % 10;
+	if(m > 5)
 		printf("last digit of %d is %d and is greater than 5",n,m);
-	else if(m==0)
+	else if(m == 0)
 		printf("last digit of %d is %d and is zero ",n,m);
-	else if(m>6 && m !=0)
+	else if(m < 6 && m != 0)
 		printf("last digit of %d is %d and is les than 6 and not zero",n,m);
 	printf("\n");
 
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	while (i < 10){
+		printf("hellow world",i);
+	}
+
+	return 0;
+}
