3dc7254280dda5ed5e5f40797d803b2aa3a45217
agutubiron@gmail.com <agutubiron@gmai.com>
2023-10-14 09:17:05 -0700

    sizes of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,15 @@
 
 int main(void)
 {
-	printf("the size of char type is %lu byte \ns",sizeof(char));
-	printf("the size of int type is %lu bytes \n",sizeof(int));
-	printf("the size of short type is %lu bytes \n",sizeof(short));
-	printf("the size of long type is %lu bytes \n",sizeof(long));
-	printf("the size of long long type is %lu bytes \n",sizeof(long long));
-	printf("the size of float type is %lu bytes \n",sizeof(float));
-	printf("the size of double is %lu bytes \n",sizeof(double));
-	printf("the size of long double is %lu bytes \n",sizeof(long double));
-	printf("the size of pointer is %lu bytes \n",sizeof(void));
+	printf("the size of char type is %lu byte \ns", sizeof(char));
+	printf("the size of int type is %lu bytes \n", sizeof(int));
+	printf("the size of short type is %lu bytes \n", sizeof(short));
+	printf("the size of long type is %lu bytes \n", sizeof(long));
+	printf("the size of long long type is %lu bytes \n", sizeof(long long));
+	printf("the size of float type is %lu bytes \n", sizeof(float));
+	printf("the size of double is %lu bytes \n", sizeof(double));
+	printf("the size of long double is %lu bytes \n", sizeof(long double));
+	printf("the size of pointer is %lu bytes \n", sizeof(void));
 
 	return (0);
 }
