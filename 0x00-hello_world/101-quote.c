1ad7459eb3ac1b272ea0fbe45914ff673cfdffdf
agutubiron@gmail.com <agutubiron@gmai.com>
2023-10-14 10:19:27 -0700

    intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,19 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * main - main function
+ * Return: 1 on success
+ */
 
+int main(void)
+{
+	const char *message = "and that piece of art is useful\" - "
 
-int main(void) {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		"Dora Korpar, 2015-10-19\n";
+
 	write(2, message, strlen(message));
+
 	return (1);
 }
 
