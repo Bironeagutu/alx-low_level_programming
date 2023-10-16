854cc18f01fb270d1017782b15aafb12b3cdabbe
agutubiron@gmail.com <agutubiron@gmai.com>
2023-10-16 07:40:28 -0700

    working with pointyers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,12 @@
-#include <stdio.h>
+#include "main.h"
 
-void reset_to_98(int *n) {
-    *n = 98;
+/**
+ * reset_to_98 - reste n to 98
+ * @n:pointer to the value to be changed
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
-
-int main() {
-    int number = 10;
-    printf("Original value: %d\n", number);
-
-    // Pass the address of 'number' to the function
-    reset_to_98(&number);
-
-    printf("Value after reset_to_98 function: %d\n", number);
-    return 0;
-}
-
