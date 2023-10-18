5bb337988135fbb8f3cf7e6eccd42acdd48d5046
agutubiron@gmail.com <agutubiron@gmai.com>
2023-10-18 15:14:58 -0700

    function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *leet(char *str);
 
 void reset_to_98(int *n);
 char *capitalize_words(char *str);
