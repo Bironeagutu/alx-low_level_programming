c83d68962cba4ca53305f966eb93a95ab169324b
agutubiron@gmail.com <agutubiron@gmai.com>
2023-10-18 14:53:42 -0700

    capitalizing letters

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,42 +7,41 @@
  */
 int is_separator(char c)
 {
-    char separators[] = " \t\n,;.!?\"(){}";
-    int i;
-    for (i = 0; separators[i] != '\0'; i++)
-    {
-        if (c == separators[i])
-        {
-            return 1;
-        }
-    }
-    return 0;
+	char separators[] = " \t\n,;.!?\"(){}";
+	int i;
+
+	for (i = 0; separators[i] != '\0'; i++)
+	{
+		if (c == separators[i])
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
 
 /**
- * cap_string - Capitalizes all words in a string based on specified separators.
+ * cap_string - Capitalizes words in a string based on specified separators.
  * @str: The input string.
  * Return: Pointer to the modified string.
  */
 char *cap_string(char *str)
 {
-    int capitalize_next = 1;
-    int i;
+	int capitalize_next = 1;
+	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
-        {
-            str[i] -= ('a' - 'A');
-        }
-        capitalize_next = 0;
-
-        if (is_separator(str[i]))
-        {
-            capitalize_next = 1;
-        }
-    }
-
-    return str;
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= ('a' - 'A');
+		}
+		capitalize_next = 0;
+		if (is_separator(str[i]))
+		{
+			capitalize_next = 1;
+		}
+	}
+	return (str);
 }
 
