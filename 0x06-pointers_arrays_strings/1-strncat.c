7caaf9eaa72e23c94746aa0be9bd3ea8ec2b549f
agutubiron@gmail.com <agutubiron@gmai.com>
2023-10-18 10:12:23 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include<string.h>
+#include "main.h"
+
+/**
+ *_strncat - Concatinates two strings
+ *@dest: destination string
+ *@src: source string
+ *@n: to ne concatinated
+ *Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = strlen(dest);
+	int i;
+
+	for (i = 0 ; i < n && *src != '\0' ; i++)
+	{
+		dest[len + i] = src[i];
+		src++;
+	}
+	dest[len + i] = '\0';
+
+	return (dest);
+}
